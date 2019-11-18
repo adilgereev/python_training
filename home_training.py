5f159eb1233cbfcb678d1a9d245e7fbc0e19edca
adilgereev <57193512+adilgereev@users.noreply.github.com>
2019-11-19 00:27:14 +0300

    Решение домашнего задания

--- home_training.py
+++ home_training.py
@@ -1,13 +1,38 @@
+import math
+
 # Домашнее задание. Урок №18
 
+# Задание №1
+# l1 = [1, 2, 3]
+# for l in l1:
+#     l2 = [i * 2 for i in l1]
+# print(l2)
+# Решено!
 
-l1 = [1, 2, 3]
+# Задание №2
+# l1 = [1, 2, 3]
+# for l in l1:
+#     l2 = [i ** 2 for i in l1]
+# print(l2, sum(l2))
+# Решено!
 
+# Задание №3
+# time1 = 3
+# time2 = 6.7
+# time3 = 11.8
+# litres = 0.5
+#
+# x1 = (math.floor(litres * time1))
+# x2 = (math.floor(litres * time2))
+# x3 = (math.floor(litres * time3))
+# print(x1, x2, x3)
+# Решено!
 
-
-print(l1)
-
-
-'''
-Проверка комита
-'''
+# Задание №4
+s = 'Hello World'
+for l in s:
+    if l == ' ':
+        print(s.upper())
+else:
+    print(s.lower())
+# Не решено!
