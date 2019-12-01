d828fab8c48b30467226d57f1c623d4d37488d7d
adilgereev <57193512+adilgereev@users.noreply.github.com>
2019-12-02 00:41:43 +0300

    Dict

---
+++ dictionary.py
@@ -0,0 +1,48 @@
+# d = {}
+# product1 = {'title': 'Sony', 'price': 100}
+# product2 = dict(title='iPhone', price=110)
+#
+# print(product1)
+# print(product2)
+
+
+# users = [
+#     ['bib@gmail.com', 'Bob'],
+#     ['katy@gmail.com', 'Katy'],
+#     ['jonh@gmail.com', 'John']
+#
+# ]
+#
+# d_users = dict(users)
+# print(users)
+# print(d_users)
+
+# product3 = dict.fromkeys(['price1', 'price2', 'price3'], 50)
+# print(product3)
+
+# nums = {i: i + 1 for i in range(1, 10)}
+# print(nums)
+
+product1 = {'title': 'Sony', 'price': 100}
+product2 = dict(title='iPhone', price=110)
+# print(product1['title'])
+# print(product1.get('title'))
+
+# for key in product1:
+#     print(f'{key}: {product1[key]}')
+
+
+# for key, value in product1.items():
+#     print(key, value)
+
+# products = [
+#     {'title': 'Sony', 'price': 100},
+#     {'title': 'iPhone', 'price': 110},
+#     {'title': 'Samsung', 'price': 90}
+#
+# ]
+# print(products)
+#
+# for product in products:
+#     print(product['title'], product['price'])
+
