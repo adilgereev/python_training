90a317324693853658a77ce065e3af17357a00d8
adilgereev <57193512+adilgereev@users.noreply.github.com>
2019-12-08 02:01:21 +0300

    Dict

--- calculator.py
+++ calculator.py
@@ -1,29 +1,40 @@
 def calculator():
     print('Приветствую на своем супер-мощном калькуляторе!')
-    x1 = int(input('Вацок, введи первое число: '))
-    print('Красиго!')
-    operator = input('А теперь введи один из знаков операции (+, -, *, /): ')
-    x2 = int(input('И второе число для операции: '))
 
-    if operator == '+':
-        print('Шикардос!')
-        print(f'{x1} + {x2} = {x1 + x2}')
-    elif operator == '-':
-        print('Четко!')
-        print(f'{x1} - {x2} = {x1 - x2}')
-    elif operator == '*':
-        print('Мощно!')
-        print(f'{x1} * {x2} = {x1 * x2}')
-    elif operator == '/':
-        print('Зачет!...')
-        print(f'{x1} / {x2} = {x1 / x2}')
+    while True:
+        x1 = int(input('Вацок, введи первое число: '))
+        print('Красиго!')
+        operator = input('А теперь введи один из знаков операции (+, -, *, /): ')
+        x2 = int(input('И второе число для операции: '))
+
+        if operator == '+':
+            print('Шикардос!')
+            print(f'{x1} + {x2} = {x1 + x2}')
+            break
+        elif operator == '-':
+            print('Четко!')
+            print(f'{x1} - {x2} = {x1 - x2}')
+            break
+        elif operator == '*':
+            print('Мощно!')
+            print(f'{x1} * {x2} = {x1 * x2}')
+            break
+        elif operator == '/':
+            print('Зачет!...')
+            print(f'{x1} / {x2} = {x1 / x2}')
+            break
+        else:
+            print('Будь по проще, уци! Вводи символы соответствующие функционалу калькулятора!')
+
 
 calculator()
 
-restart = input('Если захочешь еще какую нибудь опасную операцию провернуть, напиши слово "Еще": ')
+while True:
+    restart = input('Если хочешь еще раз провести какую нибудь опасную операцию введи "Да", если не хочешь введи "Нет": ')
 
-if restart == 'Еще' or 'Ещё':
-    calculator()
-elif restart == 'Нет':
-    print('Ну как знаешь, вацок...')
+    if restart == 'Да':
+        calculator()
+    elif restart == 'Нет':
+        print('До встречи, вацок...')
+        break
 # Почему то после слова "Нет" программа заново запускается
